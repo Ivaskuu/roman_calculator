9c15447d551719d3275770458d72c85ae225e583
Ivascu Adrian <ivascuadrian9@gmail.com>
2018-04-01 17:14:32 +0200

    Main page layout

---
+++ lib/pages/main_page/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget
+{
+  @override
+  _MainPageState createState() => new _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return new Scaffold
+    (
+      backgroundColor: Colors.white,
+      body: new Column
+      (
+        children: <Widget>
+        [
+
+        ],
+      )
+    );
+  }
+}
