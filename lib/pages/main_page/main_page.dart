d102eac9320d53ff12e79540f051c9af8046903d
Ivascu Adrian <ivascuadrian9@gmail.com>
2018-04-01 19:36:17 +0200

    Arabic to roman function

--- lib/pages/main_page/main_page.dart
+++ lib/pages/main_page/main_page.dart
@@ -93,6 +93,82 @@ class _MainPageState extends State<MainPage>
     return sum;
   }
 
+  String arabicToRoman(int arabicNum)
+  {
+    String romanNum = '';
+    do
+    {
+      if(arabicNum >= 1000)
+      {
+        romanNum += 'M';
+        arabicNum -= 1000;
+      }
+      else if(arabicNum >= 900)
+      {
+        romanNum += 'CM';
+        arabicNum -= 900;
+      }
+      else if(arabicNum >= 500)
+      {
+        romanNum += 'D';
+        arabicNum -= 500;
+      }
+      else if(arabicNum >= 400)
+      {
+        romanNum += 'CD';
+        arabicNum -= 400;
+      }
+      else if(arabicNum >= 100)
+      {
+        romanNum += 'C';
+        arabicNum -= 100;
+      }
+      else if(arabicNum >= 90)
+      {
+        romanNum += 'XC';
+        arabicNum -= 90;
+      }
+      else if(arabicNum >= 50)
+      {
+        romanNum += 'L';
+        arabicNum -= 50;
+      }
+      else if(arabicNum >= 40)
+      {
+        romanNum += 'XL';
+        arabicNum -= 40;
+      }
+      else if(arabicNum >= 10)
+      {
+        romanNum += 'X';
+        arabicNum -= 10;
+      }
+      else if(arabicNum >= 9)
+      {
+        romanNum += 'IX';
+        arabicNum -= 9;
+      }
+      else if(arabicNum >= 5)
+      {
+        romanNum += 'V';
+        arabicNum -= 5;
+      }
+      else if(arabicNum >= 4)
+      {
+        romanNum += 'IV';
+        arabicNum -= 4;
+      }
+      else if(arabicNum >= 1)
+      {
+        romanNum += 'I';
+        arabicNum -= 1;
+      }
+    }
+    while (arabicNum > 0);
+
+    return romanNum;
+  }
+
   bool checkIsRomanNumber(String number)
   {
     List romanChars = [ 'I', 'V', 'X', 'L', 'C', 'D', 'M' ];
